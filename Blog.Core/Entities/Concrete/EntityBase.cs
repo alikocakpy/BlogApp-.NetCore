abd0a523940e32ed93ca31fef5acc873adb82cce
Ali Koçak <alikocakpy@gmail.com>
2023-10-18 05:49:35 +0300

    Created DB with migration

---
+++ Blog.Core/Entities/Concrete/EntityBase.cs
@@ -0,0 +1,28 @@
+﻿using Blog.Core.Entities.Abstraction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Core.Entities
+{
+    public class EntityBase : IEntityBase
+    {
+        public EntityBase() 
+        { 
+            this.Id = Guid.NewGuid();
+            this.CreatedDate = DateTime.Now;
+            this.IsDeleted = false;
+        }
+        public Guid Id { get; set; }
+        public virtual string CreatedBy { get; set; }
+        public virtual string? ModifiedBy { get; set; }
+        public virtual string? DeletedBy { get; set; }
+        public virtual DateTime CreatedDate { get; set;}
+        public virtual DateTime ModifiedDate { get; set; }
+        public virtual DateTime DeletedDate { get; set; }
+        public virtual bool IsDeleted { get; set; }
+
+    }
+}
